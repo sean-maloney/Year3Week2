181c3bf7f1cf123ee49969a5302409b66f35385f
G00420811@atu.ie <G00420811@atu.ie>
2024-10-01 10:18:46 +0100

    Initial Commit

---
+++ src/main/java/ie/atu/year3week2/YearWeek2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.year3week2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YearWeek2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YearWeek2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/year3week2/YearWeek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.year3week2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YearWeek2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
